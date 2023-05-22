34c1c7efd8bbf0e6fa1f1d2df7fc4b9b37086c68
mayurks22 <you@example.com>
2023-05-22 14:47:52 +0530

    final project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
 const blogRouter = require('./routes/blog-routes.js');
 const router = require('./routes/user-routes.js');
 const cors = require('cors');
@@ -11,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/user', router);
 app.use('/api/blog', blogRouter);
-
 mongoose
   .connect('mongodb+srv://skothale135:e9tJpONoEqow3KuH@blogapp.79vtgfb.mongodb.net/BlogApp', {
     useNewUrlParser: true,
